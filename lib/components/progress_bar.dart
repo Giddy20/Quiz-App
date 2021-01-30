07ba9ff3625049dcc0f6f5d396229429f096e154
Gideon <gideonaigbogun17@gmail.com>
2021-01-30 23:07:45 +0100

    first commit

---
+++ lib/components/progress_bar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/state_manager.dart';
+import 'package:quiz_app/controllers/question_controller.dart';
+import 'package:websafe_svg/websafe_svg.dart';
+
+import '../constants.dart';
+
+
+class ProgressBar extends StatelessWidget {
+  const ProgressBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 35,
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+        border: Border.all(color: Color(0xFF3F4768), width: 3),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: GetBuilder<QuestionController>(
+        init: QuestionController(),
+        builder: (controller) {
+          print(controller.animation.value);
+          return Stack(
+            children: [
+              LayoutBuilder(
+                  builder: (context, constraints) => Container(
+                    width: constraints.maxWidth * controller.animation.value,
+                    decoration: BoxDecoration(
+                      gradient: kPrimaryGradient,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  )
+              ),
+              Positioned.fill(child:
+
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding / 2),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("${(controller.animation.value * 60).round()} Secs"),
+                    WebsafeSvg.asset("assets/icons/clock.svg"),
+                  ],
+                ),
+              ))
+            ],
+          );
+        }
+      ),
+    );
+  }
+}
